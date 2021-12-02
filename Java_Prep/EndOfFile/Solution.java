55e9f4b6d226f60d01c0195da6c4142a38aeacfd
June Choi <junny7020@gmail.com>
2021-12-02 21:55:31 +0000

    Dec 2nd

---
+++ Java_Prep/EndOfFile/Solution.java
@@ -0,0 +1,14 @@
+package EndOfFile;
+
+import java.util.Scanner;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i=1; sc.hasNext() == true; i++)
+        {
+            System.out.println(i + " "+ sc.nextLine());
+        }
+    }
+}
